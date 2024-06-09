17373c3035b46c540da973496d6b2393bf3aa2ce
Ismail Halili <i.halili99@hotmail.com>
2024-06-09 16:43:06 +0200

    add card

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,15 @@
 'use client'
 import { ModeToggle } from '@/components/darkmode-toggle'
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
 import { motion } from 'framer-motion'
+import { Mail } from 'lucide-react'
+import { useState } from 'react'
 
 export default function Header() {
     const containerVariants = {
@@ -56,9 +65,67 @@ export default function Header() {
                 </motion.div>
             </div>
             <div>
-                <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-                    TBD
-                </h2>
+                <motion.div animate={{}}>
+                    <Card>
+                        <CardHeader>
+                            <div className="flex items-center gap-3 mb-2">
+                                <motion.div
+                                    animate={{
+                                        rotate: [45, 405],
+                                        borderRadius: [
+                                            '0.25rem',
+                                            '0.5rem',
+                                            '0.25rem',
+                                        ],
+                                        transition: {
+                                            duration: 0.8,
+                                            repeat: Infinity,
+                                            repeatDelay: 8,
+                                        },
+                                    }}
+                                    className="border-4 border-zinc-950 dark:border-zinc-100 rounded text-zinc-200 w-[1rem] h-[1rem]"
+                                ></motion.div>
+                                <motion.p
+                                    animate={{
+                                        fontWeight: [400, 600, 400],
+                                        scaleX: [1, 1.05, 1],
+                                        transition: {
+                                            type: 'tween',
+                                            duration: 0.6,
+                                            delay: 0.4,
+                                            repeat: Infinity,
+                                            repeatDelay: 8,
+                                        },
+                                    }}
+                                    className="uppercase tracking-[0.3rem]"
+                                >
+                                    Izzy
+                                </motion.p>
+                            </div>
+                            <CardTitle>Ismail Halili, BSc.</CardTitle>
+                            <CardDescription>
+                                Masters student @ FHS (UAS Salzburg)
+                            </CardDescription>
+                            <div className="flex gap-2 pt-1 pb-1">
+                                <a href="mailto:izzy@izzyhalili.com">
+                                    <Mail></Mail>
+                                </a>
+                                <a href="https://github.com/izzyyhh">
+                                    <GitHubLogoIcon
+                                        width={24}
+                                        height={24}
+                                    ></GitHubLogoIcon>
+                                </a>
+                                <a href="https://www.linkedin.com/in/ismail-halili-46a2241b9/">
+                                    <LinkedInLogoIcon
+                                        width={24}
+                                        height={24}
+                                    ></LinkedInLogoIcon>
+                                </a>
+                            </div>
+                        </CardHeader>
+                    </Card>
+                </motion.div>
             </div>
         </header>
     )

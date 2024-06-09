b21ae66aa1c53347a84b30172532d7ba0f1c5786
Ismail Halili <i.halili99@hotmail.com>
2024-06-09 13:15:53 +0200

    init shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -19,7 +20,16 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className="text-white]">{children}</body>
+            <body className="text-white]">
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}{' '}
+                </ThemeProvider>
+            </body>
         </html>
     )
 }
